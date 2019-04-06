e1fe2bd67ff04d1688e4c66444810779703d31b4
Endel Dreyer <endel.dreyer@gmail.com>
2019-04-06 20:08:39 -0300

    fixes regexp to allow '_' character

--- src/proxy.ts
+++ src/proxy.ts
@@ -6,7 +6,7 @@ const processIds: {[id: string]: string} = {}
 const proxy = require('redbird')({
   port: Number(process.env.PORT || 80),
   resolvers: [function (host: string, url: string) {
-    const matchedProcessId = url.match(/^\/([a-zA-Z0-9\-]+)\/[a-zA-Z0-9\-]+/);
+    const matchedProcessId = url.match(/^\/([a-zA-Z0-9\-_]+)\/[a-zA-Z0-9\-_]+/);
     if (matchedProcessId && matchedProcessId[1]) {
       return processIds[matchedProcessId[1]];
     }
